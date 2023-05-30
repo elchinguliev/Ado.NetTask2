563e35bb3afbb1680c7a184783553d4defd3f037
elchinguliev <elchingulievv@gmail.com>
2023-05-31 02:50:54 +0400

    Add project files.

---
+++ Ado.NETTask2/Repos/Reposs.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.NETTask2.Repos
+{
+    public class Reposs
+    {
+        SqlConnection conn;
+        string connectionString = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        DataSet set = new DataSet();
+
+        public Reposs()
+        {
+            using (conn=new SqlConnection())
+            {
+                var sda = new SqlDataAdapter();
+                conn.ConnectionString = connectionString;
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from authors", conn);
+                sda.SelectCommand=cmd;
+                sda.Fill(set, "AuthorsSet");
+            }
+        }
+
+        public DataSet GetAll()
+        {
+            return set;
+        }
+
+        public void InsertAuthor(int Id,string Name,string Surname)
+        {
+            using (conn=new SqlConnection())
+            {
+                var cmd = new SqlCommand("Insert into authors (Id,FirstName,LastName) values(@Id,@Name,@Surname)", conn);
+                conn.ConnectionString=connectionString;
+                cmd.Parameters.Add(new SqlParameter
+                {
+                    DbType=DbType.Int16,
+                    ParameterName="@Id",
+                    Value=Id
+                }) ;
+                cmd.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType=SqlDbType.NVarChar,
+                    ParameterName="@Name",
+                    Value=Name
+                });
+                cmd.Parameters.Add(new SqlParameter
+                {
+                    DbType=DbType.Int16,
+                    ParameterName="@Surnname",
+                    Value=Surname
+                });
+
+                var sda = new SqlDataAdapter();
+                sda.InsertCommand=cmd;
+                sda.Update(set, "AuthorSet");
+                set.Clear();
+                sda=new SqlDataAdapter("select * from Authors", conn);
+                sda.Fill(set, "AuthorSet");
+            }
+        }
+        public void DeleteAuthor(int Id)
+        {
+            using (conn=new SqlConnection())
+            {
+                var cmd = new SqlCommand("Delete from authors where Id=@Id", conn);
+                conn.ConnectionString=connectionString;
+                cmd.Parameters.Add(new SqlParameter
+                {
+                    DbType=DbType.Int16,
+                    ParameterName="@Id",
+                    Value = Id
+                });
+                var sda = new SqlDataAdapter();
+                sda.DeleteCommand=cmd;
+                sda.Update(set, "AuthorSet");
+                set.Clear();
+                sda=new SqlDataAdapter("select * from Authors", conn);
+                sda.Fill(set, "AuthorSet");
+            }
+        }
+    }
+}
---
+++ Ado.NETTask2/ViewModels/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Ado.NETTask2.Commands;
+using Ado.NETTask2.Repos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.NETTask2.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+      
+     
+        public RelayCommand ShowAll { get; set; }
+        public RelayCommand InsertAuthor { get; set; }
+        public RelayCommand DeleteAuthor { get; set; }
+
+        public Reposs AuthorsReposs { get; set; }
+        public DataSet AuthorSet { get; set; } = new DataSet();
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged(); }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged(); }
+        }
+
+        private int ID;
+
+        public int SelectedId
+        {
+            get { return ID; }
+            set { ID = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            AuthorsReposs = new Reposs();
+
+            AuthorSet = AuthorsReposs.GetAll();
+
+            InsertAuthor = new RelayCommand((obj) =>
+            {
+                AuthorsReposs.InsertAuthor(Id, FirstName, LastName);
+            });
+
+            DeleteAuthor = new RelayCommand((obj) =>
+            {
+                AuthorsReposs.DeleteAuthor(SelectedId);
+            });
+
+            ShowAll = new RelayCommand((obj) =>
+            {
+                AuthorSet = AuthorsReposs.GetAll();
+            });
+        }
+    }
+}
